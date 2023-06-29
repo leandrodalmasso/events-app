4de97cd1cc7783bdd9610e31083cc471d35600e7
leandrodalmasso <leandrodalmasso@gmail.com>
2023-06-29 16:03:56 -0300

    Update newsletter endpoint to use a db

--- src/pages/api/newsletter.ts
+++ src/pages/api/newsletter.ts
@@ -1,12 +1,14 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
+import { MongoClient } from "mongodb";
+
 import { METHODS } from "../../../constants";
 
 type Data = {
   message: string;
 };
 
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
@@ -17,7 +19,18 @@ export default function handler(
       res.status(422).json({ message: "Invalid email address." });
       return;
     }
-  }
 
-  res.status(200).json({ message: "Signed up!!" });
+    const uri =
+      "mongodb+srv://leandrodalmasso:Zamba34345@cluster0.tlgtukr.mongodb.net/?retryWrites=true&w=majority";
+    const client = new MongoClient(uri);
+    await client.connect();
+    const dbName = "newsletter";
+    const collectionName = "emails";
+    const database = client.db(dbName);
+    const collection = database.collection(collectionName);
+    await collection.insertOne({ email });
+    client.close();
+
+    res.status(200).json({ message: "Signed up!!" });
+  }
 }
